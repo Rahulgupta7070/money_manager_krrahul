a856dc04d113d2fd371102e02c41418dcc1fca4d
Rahul kumar <krrahu87099@gmail.com>
2025-12-05 17:54:54 +0530

    updated HomeController

--- src/main/java/kr/rahul/moneyManager/controller/HomeController.java
+++ src/main/java/kr/rahul/moneyManager/controller/HomeController.java
@@ -1,16 +1,22 @@
 package kr.rahul.moneyManager.controller;
 
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping({"/status","/rahul"})
+@RequestMapping
+@CrossOrigin("*")
 public class HomeController {
-	
-	@GetMapping
-	public String healthCheck() {
-		return "Rahul is renuning";
-	}
+
+    @GetMapping("/health")
+    public ResponseEntity<?> checkHealth(){
+        return new ResponseEntity<>("Application is Running", HttpStatus.OK);
+    }
 
 }
